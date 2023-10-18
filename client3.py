0e63658f1bd9f59a937bc5ed29ee9fdba5ee06ff
unknown <krimbelkacem.hamnache@fgei.ummto.dz>
2023-10-19 00:31:50 +0100

    Calculate the price as the average of bid and ask prices

--- client3.py
+++ client3.py
@@ -31,11 +31,13 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+
+    # Calculate the price as the average of bid and ask prices
+    price = (bid_price + ask_price) / 2.0
+
     return stock, bid_price, ask_price, price
 
 
@@ -49,11 +51,13 @@ def getRatio(price_a, price_b):
 if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
-        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+        quotes = json.loads(urllib.request.urlopen(
+            QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" %
+                  (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % getRatio(price, price))
